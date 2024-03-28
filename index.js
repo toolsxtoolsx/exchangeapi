74a7a773d4c32f5afc401f571fbeb5a63a8ff62d
BadareOkiemuteKelvin <badareokiemute201@gmail.com>
2024-03-28 19:12:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express  = require("express")
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require('path')
+const port = process.env.PORT||3000;
+
+
+
+app.use(cors({"Access-Control-Expose-Headers": "Content-Range"}));
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+
+
+//route for post data
+app.get("/:id",(req,res)=>{
+  const id = req.params.id
+  res.redirect(301, `https://barcodescanview.com/${id}`);
+
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Examples app  very listening at http://localhost:${port}`);
+});
+
+module.exports = app;
